b47ea9b8ac0ac4f84551404439f0d048ceb44398
Jared Truscott <Jtruscott78@gmail.com>
2017-12-21 15:14:21 -0500

    rewrote database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220202854) do
+ActiveRecord::Schema.define(version: 20171221190603) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
