8f6b7eb2f6985eace0d5da7741dedb7a9663ce4f
Jared Truscott <Jtruscott78@gmail.com>
2017-12-22 20:23:36 -0500

    added edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,34 @@ class ArticlesController < ApplicationController
   end
 
   def create
-
     @article = Article.new(article_params)
 
     if @article.save
       flash[:notice] = 'Article was successfully created.'
       redirect_to article_path(@article)
     else
-      render :new
+      render 'new'
     end
   end
 
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
 
   private
     def article_params
