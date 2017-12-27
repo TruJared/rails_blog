5882508d8f8259279d6a8db9dcff37324a6ec2b8
Jared Truscott <Jtruscott78@gmail.com>
2017-12-26 20:17:42 -0500

    removed reduncancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
+# main controller
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-  end
+  before_action :set_article, only: %i[edit update show destroy]
 
   def index
     @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
   end
 
   def create
@@ -18,16 +23,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = 'Article successfully updated'
       redirect_to article_path(@article)
@@ -37,15 +35,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-
     flash[:notice] = 'Article Was Deleted'
     redirect_to articles_path
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
